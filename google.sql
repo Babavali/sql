5a20360a945d3cdc91fec8feaa08f6291bccafc9
Babavali <valibab@gmail.com>
2025-06-24 16:59:22 +0530

    Update google.sql

--- google.sql
+++ google.sql
@@ -13,9 +13,5 @@ CREATE TABLE `AdGroupAdLegacy` (
     `metrics_ctr` FLOAT DEFAULT 0,
     `metrics_cost_per_conversion` FLOAT DEFAULT 0,
     `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
-    `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (`customer_id`) REFERENCES `Customer`(`customer_id`),
-    FOREIGN KEY (`campaign_id`) REFERENCES `Campaign`(`campaign_id`),
-    FOREIGN KEY (`ad_group_id`) REFERENCES `AdGroup`(`ad_group_id`),
-    UNIQUE KEY `unique_ad_metrics` (`customer_id`, `campaign_id`, `ad_group_id`, `ad_group_ad_ad_id`, `report_date`)
+    `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
