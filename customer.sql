2acb82731e83c2e6176b7560504cd938c72a3efe
Babavali <valibab@gmail.com>
2025-06-27 11:41:42 +0530

    Update customer.sql

--- customer.sql
+++ customer.sql
@@ -1,22 +1,23 @@
 -- Customer table schema
-CREATE TABLE customer (
-    accountId INT PRIMARY KEY,
-    addsetId INT,
-    doctorId INT,
-    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-   );
+--DROP TABLE IF EXISTS `customer`;
+CREATE TABLE `customer` (
+    `accountId` INT PRIMARY KEY,
+    `addsetId` INT,
+    `doctorId` INT,
+    `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
 
 -- Leads table schema
-CREATE TABLE leads (
-    Id INT PRIMARY KEY,
-    accountId INT,
-    addsetId INT,
-    doctorId INT,
-    fieldData TEXT, -- Assuming fieldData is a text field; adjust type as needed
-    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (accountId) REFERENCES customer(accountId),
-    FOREIGN KEY (doctorId) REFERENCES customer(doctorId)
-
-   );
+--DROP TABLE IF EXISTS `leads`;
+CREATE TABLE `leads` (
+    `Id` INT PRIMARY KEY,
+    `accountId` INT,
+    `addsetId` INT,
+    `doctorId` INT,
+    `fieldData` TEXT, -- Assuming fieldData is a text field; adjust type as needed
+    `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (`accountId`) REFERENCES `customer`(`accountId`),
+    FOREIGN KEY (`doctorId`) REFERENCES `customer`(`doctorId`)
+);
