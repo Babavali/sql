324bf9818e2c7a4d802871f3a2f5a8ec0d3422e4
Babavali <valibab@gmail.com>
2025-07-11 18:46:56 +0530

    Update client.sql

--- client.sql
+++ client.sql
@@ -1,12 +1,14 @@
 -- DROP TABLE IF EXISTS `client_website_page_queries`;
 CREATE TABLE `client_website_page_queries` (
-    `id` INT PRIMARY KEY ,
-    `client_website_id` INT ,
-    `client_website_page_id` INT ,
+    `id` INT PRIMARY KEY,
+    `clientWebsiteId` INT,
+    `clientWebsitePageId` INT,
     `query` TEXT,
-    `clicks` INT ,
-    `impressions` INT ,
-    `extracted_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
-    `extracted_to` DATETIME ,
-    `extracted_from` DATETIME
+    `clicks` INT,
+    `impressions` INT,
+    `extractedAt` DATETIME DEFAULT CURRENT_TIMESTAMP,
+    `extractedTo` DATETIME,
+    `extractedFrom` DATETIME,
+    `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
