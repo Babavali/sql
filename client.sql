98283e13c804caa5f23859d4d6164b37c6581bb3
Babavali <valibab@gmail.com>
2025-07-11 18:23:16 +0530

    Update client.sql

--- client.sql
+++ client.sql
@@ -1,12 +1,12 @@
 -- DROP TABLE IF EXISTS `client_website_page_queries`;
 CREATE TABLE `client_website_page_queries` (
-    `id` INT PRIMARY KEY AUTO_INCREMENT,
-    `client_website_id` INT NOT NULL,
-    `client_website_page_id` INT NOT NULL,
-    `query` VARCHAR(255) NOT NULL,
-    `clicks` INT DEFAULT 0,
-    `impressions` INT DEFAULT 0,
+    `id` INT PRIMARY KEY ,
+    `client_website_id` INT ,
+    `client_website_page_id` INT ,
+    `query` TEXT,
+    `clicks` INT ,
+    `impressions` INT ,
     `extracted_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
-    `extracted_to` DATETIME,
+    `extracted_to` DATETIME ,
     `extracted_from` DATETIME
 );
