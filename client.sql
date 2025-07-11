2a3fc3ae4605f6d2cb23f816ba95d90bcab7632b
Babavali <valibab@gmail.com>
2025-07-11 19:10:37 +0530

    Update client.sql

--- client.sql
+++ client.sql
@@ -12,3 +12,13 @@ CREATE TABLE `clientwebsitepagequeries` (
     `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
     `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
+
+-- Customer table schema
+--DROP TABLE IF EXISTS `customer`;
+CREATE TABLE `clientwebsitepagequeries` (
+    `accountId` INT PRIMARY KEY,
+    `addsetId` INT,
+    `doctorId` INT,
+    `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
