1765ac16200e53c4b7676c9e92942c1d5b6e01a7
Babavali <valibab@gmail.com>
2025-07-11 18:56:52 +0530

    Update customer.sql

--- customer.sql
+++ customer.sql
@@ -7,3 +7,17 @@ CREATE TABLE `customer` (
     `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
     `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
+
+-- Leads table schema
+--DROP TABLE IF EXISTS `leads`;
+CREATE TABLE `leads` (
+    `Id` INT PRIMARY KEY,
+    `accountId` INT,
+    `addsetId` INT,
+    `doctorId` INT,
+    `fieldData` TEXT, -- Assuming fieldData is a text field; adjust type as needed
+    `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (`accountId`) REFERENCES `customer`(`accountId`),
+    FOREIGN KEY (`doctorId`) REFERENCES `customer`(`doctorId`)
+);
