2a92df42011a3410473afae9971a32d2bbb4a055
Babavali <valibab@gmail.com>
2025-07-11 18:21:22 +0530

    Update customer.sql

--- customer.sql
+++ customer.sql
@@ -22,16 +22,5 @@ CREATE TABLE `leads` (
     FOREIGN KEY (`doctorId`) REFERENCES `customer`(`doctorId`)
 );
 
--- DROP TABLE IF EXISTS `client_website_page_queries`;
-CREATE TABLE `client_website_page_queries` (
-    `id` INT PRIMARY KEY AUTO_INCREMENT,
-    `client_website_id` INT NOT NULL,
-    `client_website_page_id` INT NOT NULL,
-    `query` VARCHAR(255) NOT NULL,
-    `clicks` INT DEFAULT 0,
-    `impressions` INT DEFAULT 0,
-    `extracted_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
-    `extracted_to` DATETIME,
-    `extracted_from` DATETIME
-);
+
 
