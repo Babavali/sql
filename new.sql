9b52941ce4e52571cb874c9f88de0b17c6158fac
Babavali <valibab@gmail.com>
2025-07-11 19:01:41 +0530

    Update new.sql

--- new.sql
+++ new.sql
@@ -23,3 +23,18 @@ CREATE TABLE `PhotoFilters` (
     `filterVersion` VARCHAR(20),
     FOREIGN KEY (`contentId`) REFERENCES `MediaContent`(`contentId`) ON DELETE CASCADE
 );
+
+-- DROP TABLE IF EXISTS `client_website_page_queries`;
+CREATE TABLE `clientwebsitepagequeries` (
+    `id` INT PRIMARY KEY,
+    `clientWebsiteId` INT,
+    `clientWebsitePageId` INT,
+    `query` TEXT,
+    `clicks` INT,
+    `impressions` INT,
+    `extractedAt` DATETIME DEFAULT CURRENT_TIMESTAMP,
+    `extractedTo` DATETIME,
+    `extractedFrom` DATETIME,
+    `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
