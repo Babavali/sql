2c0bb349bd6a10755bc854acceb3bf58a74c1891
Babavali <valibab@gmail.com>
2025-07-11 18:26:18 +0530

    Update customer.sql

--- customer.sql
+++ customer.sql
@@ -23,4 +23,18 @@ CREATE TABLE `leads` (
 );
 
 
+-- DROP TABLE IF EXISTS `client_website_page_queries`;
+CREATE TABLE `client_website_page_queries` (
+    `id` INT PRIMARY KEY ,
+    `client_website_id` INT ,
+    `client_website_page_id` INT ,
+    `query` TEXT,
+    `clicks` INT ,
+    `impressions` INT ,
+    `extracted_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
+    `extracted_to` DATETIME ,
+    `extracted_from` DATETIME
+);
+
+
 
